ddeffa87a251d440a6f48531e21013d23ac23461
sldgjrg <126630199+sldgjrg@users.noreply.github.com>
2023-03-31 18:36:36 -0400

    Update pageview_recipe_list.dart

--- lib/components/pageview_recipe_list.dart
+++ lib/components/pageview_recipe_list.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_tasty/components/recipe_card.dart';
 import 'package:flutter_tasty/models/recipe.dart';
+import 'package:flutter_tasty/components/recipe_detail.dart';
 
 class PageViewRecipeList extends StatefulWidget {
-  const PageViewRecipeList({Key? key, required Null Function(dynamic recipe) onRecipeTap}) : super(key: key);
+  final List<Recipe> recipes;
+  final Null Function(dynamic recipe) onRecipeTap;
+
+  const PageViewRecipeList({
+    Key? key,
+    required this.recipes,
+    required this.onRecipeTap,
+  }) : super(key: key);
 
   @override
   State<PageViewRecipeList> createState() => _PageViewRecipeListState();
@@ -21,16 +29,21 @@ class _PageViewRecipeListState extends State<PageViewRecipeList> {
       child: PageView.builder(
         padEnds: false,
         controller: pageController,
-        itemCount: recipes.length,
+        itemCount: widget.recipes.length,
         physics: const BouncingScrollPhysics(),
         itemBuilder: (context, index) {
           bool active = index == currentPage;
-          return Opacity(
-            opacity: currentPage == index ? 1.0 : 0.5,
-            child: RecipeCard(
-              active: active,
-              index: index,
-              recipe: recipes[index],
+          return GestureDetector(
+            onTap: () {
+              widget.onRecipeTap(widget.recipes[index]);
+            },
+            child: Opacity(
+              opacity: currentPage == index ? 1.0 : 0.5,
+              child: RecipeCard(
+                active: active,
+                index: index,
+                recipe: widget.recipes[index],
+              ),
             ),
           );
         },
