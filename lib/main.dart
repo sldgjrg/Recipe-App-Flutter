003f5f29b0f67c40117b43b535f84dd1efc2f1df
sldgjrg <126630199+sldgjrg@users.noreply.github.com>
2023-04-01 12:53:25 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_tasty/screens/home_screen.dart';
+import 'package:flutter_tasty/components/recipe_detail.dart';
 
+import 'models/recipe.dart';
 import 'screens/onboarding_screen.dart';
 
 void main() {
@@ -21,6 +23,15 @@ class App extends StatelessWidget {
         ),
       ),
       home: const OnboardingScreen(),
+      onGenerateRoute: (settings) {
+        if (settings.name == '/recipe_detail') {
+          final value = settings.arguments as Recipe; // Retrieve the value.
+          return MaterialPageRoute(
+              builder: (_) => RecipeDetailPage(
+                  recipe: value)); // Pass it to RecipeDetailPage.
+        }
+        return null; // Let `onUnknownRoute` handle this behavior.
+      },
     );
   }
 }
