2874ce56fb5ae8d49e8fb742aebcde5871c56a2d
sldgjrg <126630199+sldgjrg@users.noreply.github.com>
2023-04-01 12:53:40 -0400

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,15 +3,7 @@ import 'package:flutter_svg/svg.dart';
 import 'package:flutter_tasty/components/my_bottom_navigation_bar.dart';
 import 'package:flutter_tasty/components/pageview_recipe_list.dart';
 import 'package:flutter_tasty/components/popular_recipe_list.dart';
-import 'package:flutter_tasty/components/recipe_detail.dart';
 import 'package:flutter_tasty/models/recipe.dart';
-
-class RecipeDetailArguments {
-  final Recipe recipe;
-  static const routeName = '/recipe_detail';
-
-  RecipeDetailArguments({required this.recipe});
-}
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -63,50 +55,36 @@ class MySearchDelegate extends SearchDelegate<String> {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xFFe7eefb),
-      body: Column(
-        children: [
-          GestureDetector(
-            onTap: () {
-              Navigator.pushNamed(
-                context,
-                RecipeDetailArguments.routeName, // use the named route
-                arguments: RecipeDetailArguments(
-                  recipe: recipes[0],
-                ),
-              );
-            },
-            child: Expanded(
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: const Color(0xFFe7eefb),
+        body: Column(
+          children: [
+            Expanded(
               child: PageViewRecipeList(
-                recipes: const [],
-                onRecipeTap: (recipe) {},
+                recipes: recipes,
+                onRecipeTap: (recipe) {
+                  Navigator.pushNamed(
+                    context,
+                    RecipeDetailArguments.routeName, // use the named route
+                    arguments: recipe,
+                  );
+                },
               ),
             ),
-          ),
-          GestureDetector(
-            onTap: () {
-              Navigator.pushNamed(
-                context,
-                RecipeDetailArguments.routeName, // use the named route
-                arguments: RecipeDetailArguments(
-                  recipe: recipes[1],
-                ),
-              );
-            },
-            child: const PopularRecipeList(),
-          ),
-        ],
+            const Expanded(child: PopularRecipeList()),
+          ],
+        ),
+        bottomNavigationBar: const MyBottomNavigationBar(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showSearch(context: context, delegate: MySearchDelegate());
+          },
+          backgroundColor: Colors.orange,
+          child: const Icon(Icons.search),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       ),
-      bottomNavigationBar: const MyBottomNavigationBar(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showSearch(context: context, delegate: MySearchDelegate());
-        },
-        backgroundColor: Colors.orange,
-        child: const Icon(Icons.search),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
